48e4cf032163ed5ecfeee7dc1386f8e03389e951
ahkamulbd <ahkamulbd@gmail.com>
2024-02-22 21:03:36 +0600

    Card border radius set 30px with javaScript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Query 1:
+// Query 1:নতুন একটা স্ক্রিপ্ট ফাইল খুলবে। সেটাকে Index.Html এর সাথে কানেক্ট করতে পারো কিনা?
 //console.log('This is script file.');
 
-// Query 2: 
+// Query 2: যতগুলা ইলিমেন্টে H2 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার কালার Lightblue করেদিতে পারো কিনা। 
 
 const h2Tag = document.querySelectorAll('h2');
 //h2Tag.style.color = 'blue';
@@ -9,4 +9,16 @@ const h2Tag = document.querySelectorAll('h2');
 for (let h2 of h2Tag) {
     //console.log(h2.innerText);
     h2.style.color = 'lightblue';
+}
+
+// Query 3: ওই ওয়েবসাইট এ Backpack আইডি দিয়ে একটা সেকশন আছে। সেই সেকশনের ব্যাকগ্রাউন্ড কালার Tomato করে দিতে পারো কিনা। 
+
+const backpackSection = document.getElementById('backpack');
+backpackSection.style.backgroundColor = 'tomato';
+
+// Query 4: ওই ওয়েবসাইট এ Backpack আইডি দিয়ে একটা সেকশন আছে। সেই সেকশনের ব্যাকগ্রাউন্ড কালার Tomato করে দিতে পারো কিনা। 
+
+const cards = document.getElementsByClassName('card');
+for (let card of cards) {
+    card.style.borderRadius = '30px';
 }
